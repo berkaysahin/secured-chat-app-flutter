47f91282192bd657b988a0bed03ee1e40ea636aa
Berkay Şahin <berkayshn97@gmail.com>
2021-11-10 22:33:32 +0300

    update friend api urls

--- lib/services/urls.dart
+++ lib/services/urls.dart
@@ -2,11 +2,11 @@ import 'package:secured_chat_app/services/domain_url.dart';
 
 String urlLogin = domain + "/api/v1/user/login";
 String urlRegister = domain + "/api/v1/user/register";
-String urlAddFriend = domain + "/api/v1/user/addfriend";
-String urlGetAddFriendRequests = domain + "/api/v1/user/getaddfriendrequests";
+String urlAddFriend = domain + "/api/v1/friend/addfriend";
+String urlGetAddFriendRequests = domain + "/api/v1/friend/getaddfriendrequests";
 String urlAcceptFriendRequests =
-    domain + "/api/v1/user/acceptaddfriendrequests";
+    domain + "/api/v1/friend/acceptaddfriendrequests";
 String urlRejectFriendRequests =
-    domain + "/api/v1/user/rejectaddfriendrequests";
-String urlGetFriends = domain + "/api/v1/user/GetFriends";
-String urlRemoveFriend = domain + "/api/v1/user/RemoveFriend";
+    domain + "/api/v1/friend/rejectaddfriendrequests";
+String urlGetFriends = domain + "/api/v1/friend/GetFriends";
+String urlRemoveFriend = domain + "/api/v1/friend/RemoveFriend";
